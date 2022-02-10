f6ad70cb1a2260daf3bd1c390d48158ea6673d4b
Fe1ixJ <66786588+Fe1ixJ@users.noreply.github.com>
2022-02-10 09:24:56 +0100

    Add files via upload

---
+++ 13.1/Cirkel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace _13._1
+{
+    class Cirkel
+    {
+        private int x = 0;
+        private int y = 0;
+        private int diameter = 40;
+
+        public Cirkel(int x, int y, int diameter)
+        {
+            X = x;
+            Y = y;
+            diameter = diameter;
+        }
+        #region Egenskaper
+        public int X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+        public int Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+        public int Diameter
+        {
+            get { return diameter; }
+            set { if (value < 0) diameter = -value; else diameter = value; }
+        }
+        #endregion
+
+        public void Rita(Graphics g)
+        {
+            g.FillEllipse(new SolidBrush(Color.Red), X, Y, Diameter, Diameter);
+        }
+
+        public bool ÄrTräffad(int musX, int MusY)
+        {
+            bool träffad = false;
+            int dx = musX - (X+Diameter/2);
+            int dy = MusY - (Y+Diameter/2);
+            int ds2 = dx*dx + dy*dy;
+            if (ds2 < (Diameter/2) * (diameter/2))
+            {
+                träffad = true;
+
+            }
+            return träffad;
+        }
+    }
+
+
+}
---
+++ 13.1/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _13._1
+{
+    public partial class Form1 : Form
+    {
+        private List<Cirkel> cirklar = new List<Cirkel>();
+        private bool speletÄrSlut = false;
+        public Form1()
+        {
+            InitializeComponent();
+            for (int rad = 0; rad <4; rad++)
+            {
+                for (int kolumn = 0; kolumn < 6; kolumn++)
+                {
+                    int x = 10 + 50 * kolumn;
+                    int y = 10 + 50 * rad;
+                    int diameter = 20;
+                    Cirkel cirkel = new Cirkel(x, y, diameter);
+                    cirklar.Add(cirkel);
+                }
+            }
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            for (int i = 0; i < cirklar.Count; i++)
+            {
+                Cirkel cirkel = cirklar.ElementAt(i);
+                cirkel.Rita(g);
+            }
+            if (speletÄrSlut)
+            {
+                string text = "grattis";
+                Font font = new Font("Arial", 30);
+                SolidBrush brush = new SolidBrush(Color.Blue);
+                int x = 25;
+                int y = 30;
+                g.DrawString(text, font, brush, x, y);
+            }
+        }
+
+        protected override void OnMouseClick(MouseEventArgs e)
+        {
+            bool träff = false;
+            for (int i = 0; i<cirklar.Count && !träff; i++)
+            {
+                Cirkel cirkel = cirklar.ElementAt(i);
+                if (cirkel.ÄrTräffad(e.X, e.Y))
+                {
+                    cirklar.RemoveAt(i);
+                    träff = true;
+                }
+            }
+            if (cirklar.Count == 0)
+            {
+                speletÄrSlut = true;
+            }
+            Invalidate();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
